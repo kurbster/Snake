cfe42fdf9033d6fe039c4af645edf9570fa22688
kurbster <kkuznia28@gmail.com>
2020-07-18 18:41:33 -0700

    Added the main menu loop
    Still not done I need to add text to the buttons

--- Snake.py
+++ Snake.py
@@ -24,10 +24,14 @@ columns = width // box_size
 screen = pg.display.set_mode((width, height))
 
 # Adding the title and icons
+pg.init()
 pg.display.set_caption('Snake Game')
-icon = pg.image.load('png/002-cobra.png')
+icon = pg.image.load('002-cobra.png')
 pg.display.set_icon(icon)
 clock = pg.time.Clock()
+
+# Font objects 
+font = pg.font.SysFont('ariel', 20)
 
 # This is the draw function that will get called every frame of the game
 def draw_grid():
@@ -57,6 +61,12 @@ def draw_snake():
 def draw_food():
     pg.draw.rect(screen, red, (food[0] * box_size, food[1] * box_size, box_size, box_size))
 
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, 1, color)
+    text_rect = text_obj.get_rect()
+    text_rect.topleft = (x, y)
+    surface.blit(text_obj, text_rect)
+    
 # This method is here so after the player enters the end game state they 
 # Can choose to replay the game therefore this method will initialize
 # The game and its default values before returning and letting the game
@@ -80,9 +90,10 @@ def init_game():
     food_x = 0
     food_y = 0
     food = [food_x, food_y]
-    
-
+   
+  
 def run_game():
+    init_game()
     # I must pass in my global variables
     global snake, food, snake_x_change, snake_y_change
     # This is our game loop
@@ -150,14 +161,38 @@ def end_game():
     while True:
         pass
     
+click = False
 # This is my main menu loop where I will add buttons so the
 # User can choose to play the game or have an AI play it(Version 2)
 def main_loop():
+    global click, font
     while True:
         screen.fill(black)
-    
-init_game()
-run_game()
+        draw_text('main menu', font, white, screen, 20, 20)
+        mx, my = pg.mouse.get_pos()
+        
+        draw_text('play game', font, white, screen, 50, 100)
+        play_button = pg.Rect(50, 100, 200, 50)
+        
+        # ai_button = pg.Rect(50, 200, 200, 50)
+        if play_button.collidepoint((mx, my)):
+            if click:
+                run_game()
+        pg.draw.rect(screen, red, play_button)
+        
+        click = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        
+        pg.display.update()
+        clock.tick(60)
+        
+main_loop()
 
     
     
